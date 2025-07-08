b438bdb38abfe290ffa48dc3d4c5bea935bfee1a
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-07-08 23:48:59 +0200

    chore: Refactored classnames

--- src/page/NotFoundPage.tsx
+++ src/page/NotFoundPage.tsx
@@ -6,11 +6,11 @@ import styles from './NotFoundPage.module.less';
 const NotFoundPage = () => {
   return (
     <main className={styles.pageContainer}>
-      <div className={styles.header}>
-        <h1 className={styles.pageTitle}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
           Well, this isn’t where we were supposed to end up
         </h1>
-        <p className={styles.pageParagraph}>
+        <p className={styles.paragraph}>
           Looks like we took a wrong turn somewhere and landed on this empty
           page. Not exactly what you were hoping for, right? Hit the button
           below, and we’ll whisk you away to the comfort of the homepage.
